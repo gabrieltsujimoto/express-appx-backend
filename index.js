a1fb6d799a17ab60ed0ce8d9a2244e7a9dc61497
gabrieltsujimoto <gabrieltsujimotovicente@outlook.com>
2024-03-25 16:56:34 -0300

    update

--- index.js
+++ index.js
@@ -1,20 +1,16 @@
 const express = require('express')
 const app = express();
 
-app.listen(3000, () => console.log(`
-    -----------------------------
-    -----------------------------
-    ------ BACKEND RODANDO ------
-    -----------------------------
-    -----------------------------
-`))
-
-
+//Middleware function
+app.use('/home', (req, res, next) => {
+    res.send('Antes do get')
+    next()
+})
 
 // Metódos são use, all (pra todos os verbos do http) e get/put/etc para especificos
-app.get('/home', (req, res) => {
+app.get('/home', (req, res, next) => {
     // res.send('Estou <b>rodando!</b>')
-
+    console.log('Durante o get...')
     res.json({
         data: [
             { id: 7, name: 'Bia', position: 1 },
@@ -27,7 +23,9 @@ app.get('/home', (req, res) => {
         status: 200
     })
 
-    // res.json([
+    next()
+
+    /* res.json([
     //     {id: 7, name: 'Bia', position: 1},
     //     {id: 34, name: 'Ana', position: 2},
     //     {id: 47, name: 'Carlos', position: 3}
@@ -37,12 +35,19 @@ app.get('/home', (req, res) => {
     //     name: 'iPad 32GB',
     //     price: 1899.00,
     //     discount: 0.12
-    // })
+    // })*/
 })
-
 
 //Middleware function
-app.use('/home', (req, res) => {
-    res.send('Caiu no middleware')
-    console.log('Caiu no middleware! ')
+app.use('/home', (req, res, next) => {
+    res.send('Depois do get')
+    next()
 })
+
+app.listen(3000, () => console.log(`
+    -----------------------------
+    -----------------------------
+    ------ BACKEND RODANDO ------
+    -----------------------------
+    -----------------------------
+`))
